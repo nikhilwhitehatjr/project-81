b94ee61036fd1895bfe91cf1cbb0c0bee516e266
nikhilwhitehatjr <72554162+nikhilwhitehatjr@users.noreply.github.com>
2021-05-19 17:55:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 4;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
